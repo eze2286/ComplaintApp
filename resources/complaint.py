ed7ce577759e59086285d868b7e19e8415abac69
Ezequiel Saldaña <eze2286@hotmail.com>
2023-06-24 20:37:50 -0300

    Testing black formatter

--- resources/complaint.py
+++ resources/complaint.py
@@ -10,30 +10,50 @@ from schemas.response.complaint import ComplaintOut
 
 router = APIRouter(tags=["Complaints"])
 
-@router.get("/complaints/", dependencies=[Depends(oauth2_scheme)], response_model=List[ComplaintOut])
-async def get_complaints(request:Request):
+
+@router.get(
+    "/complaints/",
+    dependencies=[Depends(oauth2_scheme)],
+    response_model=List[ComplaintOut],
+)
+async def get_complaints(request: Request):
     user = request.state.user
     print(user["role"])
     return await ComplaintManager.get_complaints(user)
 
-@router.post("/complaints/", dependencies=[Depends(oauth2_scheme), Depends(is_complainer)],
-response_model=ComplaintOut)
+
+@router.post(
+    "/complaints/",
+    dependencies=[Depends(oauth2_scheme), Depends(is_complainer)],
+    response_model=ComplaintOut,
+)
 async def create_complaint(request: Request, complaint: ComplaintIn):
-    user = request.state.user 
+    user = request.state.user
     return await ComplaintManager.create_complaint(complaint, user)
 
-@router.delete("/complaints/{complaint_id}", dependencies=[Depends(oauth2_scheme), Depends(is_admin)],
-        status_code=204)
-async def delete_complaint(complaint_id:int):
+
+@router.delete(
+    "/complaints/{complaint_id}",
+    dependencies=[Depends(oauth2_scheme), Depends(is_admin)],
+    status_code=204,
+)
+async def delete_complaint(complaint_id: int):
     await ComplaintManager.delete(complaint_id)
 
-@router.put("/complaints/{complaint_id}/approve", dependencies=[Depends(oauth2_scheme), Depends(is_approver)],
-            status_code=204)
-async def approve_complaint(complaint_id:int):
+
+@router.put(
+    "/complaints/{complaint_id}/approve",
+    dependencies=[Depends(oauth2_scheme), Depends(is_approver)],
+    status_code=204,
+)
+async def approve_complaint(complaint_id: int):
     await ComplaintManager.approve(complaint_id)
 
-@router.put("/complaints/{complaint_id}/reject", dependencies=[Depends(oauth2_scheme), Depends(is_approver)],
-            status_code=204)
-async def approve_complaint(complaint_id:int):
-    await ComplaintManager.reject(complaint_id)
 
+@router.put(
+    "/complaints/{complaint_id}/reject",
+    dependencies=[Depends(oauth2_scheme), Depends(is_approver)],
+    status_code=204,
+)
+async def approve_complaint(complaint_id: int):
+    await ComplaintManager.reject(complaint_id)
