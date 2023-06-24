792cc7c613ae5dc5c3fd758d675bd033b29cc584
Ezequiel Saldaña <eze2286@hotmail.com>
2023-06-24 19:58:10 -0300

    testing black file in repository

--- resources/complaint.py
+++ resources/complaint.py
@@ -17,7 +17,7 @@ async def get_complaints(request:Request):
     return await ComplaintManager.get_complaints(user)
 
 @router.post("/complaints/", dependencies=[Depends(oauth2_scheme), Depends(is_complainer)],
-             response_model=ComplaintOut)
+response_model=ComplaintOut)
 async def create_complaint(request: Request, complaint: ComplaintIn):
     user = request.state.user 
     return await ComplaintManager.create_complaint(complaint, user)
